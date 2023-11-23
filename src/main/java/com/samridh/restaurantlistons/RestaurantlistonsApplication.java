28113e66f560520abfe4fb90d8b43f0233dd8464
samridh-sinha <samridh.sinha@techverito.com>
2023-11-23 16:49:55 +0530

    Initial commit

---
+++ src/main/java/com/samridh/restaurantlistons/RestaurantlistonsApplication.java
@@ -0,0 +1,13 @@
+package com.samridh.restaurantlistons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantlistonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantlistonsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/samridh/restaurantlistons/RestaurantlistonsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samridh.restaurantlistons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantlistonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

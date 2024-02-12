c2d1ef322bdf9dfa8378509f879a2b181f09983f
Samridh Sinha <samridh.sinha@techverito.com>
2024-02-12 16:18:17 +0530

    #Modify: Modifies the restaurant listing services files

--- src/main/java/com/samridh/restaurantlistons/RestaurantListingApplication.java
+++ src/main/java/com/samridh/restaurantlistons/RestaurantListingApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RestaurantlistonsApplication {
+
+public class RestaurantListingApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RestaurantlistonsApplication.class, args);
+		SpringApplication.run(RestaurantListingApplication.class, args);
 	}
 
 }
